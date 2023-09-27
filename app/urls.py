00a7f70e6acaeae66e6910092e6c414446a581ef
Augustodalmas <augustodalmas132@gmail.com>
2023-09-27 14:47:45 -0300

    Adicionado Views, URL e Template ao projeto

--- app/urls.py
+++ app/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#Esta é a camada onde orientamos nossas URLS do site
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_views),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+#Esta camada será mostrada ao usuário, como o nome já diz
+def cars_views(request):
+    return render(request,
+                'cars.html',
+                {'cars' : {'model' : 'astra 2.0'}} #Informação enviada pelo context ao meu HTML
+                )
 
-# Create your views here.
+#Para não digitar todo o HTML nessa pagina, utilizamos os 'templates' com RENDER
