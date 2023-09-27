9f201af7fc8beaf1ee7e955ea5c274a0a35d10d5
Augustodalmas <augustodalmas132@gmail.com>
2023-09-27 10:38:23 -0300

    Adicionado Models e Admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from cars.models import Cars, Brand
 
-# Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class CarAdmin(admin.ModelAdmin):
+    #List_display, será os itens que aparecerão no topo de nossa grade
+    list_display = ("model", "brand", "factory_year", "model_year", "value")
+    #search_fields, são os campos quais podemos filtrar
+    search_fields = ("model", "brand")
+
+#Aqui estamos dizendo ao django, que queremos que o modelo Cars, receba a classe CarAdmin como seu filtro de admin.
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Cars, CarAdmin)
+
+
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+"""
+Este arquivo é onde mostramos ao django, um modelo que desejamos criar dentro do banco de dados, tipando seus nomes.
+"""
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, blank=False, null=False)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+
+class Cars(models.Model):
+    """
+    primary_key = chave primaria, onde tudo se referencia
+    Max_length = tamanho máximo de palavras
+    Blank = opção de deixar em branco
+    Null = opção de deixar nulo
+    AutoFiel = auto-incrementar
+    CharField = campo de texto
+    IntegerField = campo numérico
+    FloatFiel = campo de ponto flutuante
+    Foreignkey = chave de ligação entre tabelas
+    On_delete, serve para dar uma ação quando tentar deletar uma tabela, neste caso, proteger e nao permitir
+    Related_name, nome de relação na conexão de tabelas
+    ImageField, é um campo de imagens, para botar imagem no produto
+    Upload_to, é a referencia para onde vamos dar upload da imagem com django
+    """
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="car_brand")
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='car/', blank=True, null=True)
+
+#Para adicionar ao banco de dados, utilizar "Makimigrations" e após "migrate"
+    def __str__(self):
+        return self.model
