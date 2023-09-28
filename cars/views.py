f1e3863b3194141261b99e3c7001255eb6a3c966
Augustodalmas <augustodalmas132@gmail.com>
2023-09-28 10:23:34 -0300

    Adicionado HTML Base, Template e ajuste nas Views

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from cars.models import Cars
 #Esta camada será mostrada ao usuário, como o nome já diz
 def cars_views(request):
-    return render(request,
-                'cars.html',
-                {'cars' : {'model' : 'astra 2.0'}} #Informação enviada pelo context ao meu HTML
-                )
+    cars = Cars.objects.all().order_by('model')
+    search = request.GET.get('search') #pega a informação enviada pelo usuário
+    if search:
+        cars = Cars.objects.filter(model__icontains=search) #Orms do Django
+
+    return render(
+        request,
+        'cars.html',
+        {'cars' : cars} #Informação que está vindo do meu models
+        )
 
 #Para não digitar todo o HTML nessa pagina, utilizamos os 'templates' com RENDER
