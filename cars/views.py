918901a0f47fe6d61a624b61774ee62429f1b5c1
Augustodalmas <augustodalmas132@gmail.com>
2023-09-28 16:01:08 -0300

    Adicionado Forms,Adicionar carros e nova janela

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Cars
+from cars.forms import CarModelForm
 #Esta camada será mostrada ao usuário, como o nome já diz
 def cars_views(request):
     cars = Cars.objects.all().order_by('model')
@@ -14,3 +15,13 @@ def cars_views(request):
         )
 
 #Para não digitar todo o HTML nessa pagina, utilizamos os 'templates' com RENDER
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
+    return render(request,'new_car.html',{'new_car_form' : new_car_form})
